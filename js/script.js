f29157f5c1337f2d75fe9f3881b33e1f8200a4d2
OsmanMariam <sufimariam8@gmail.com>
2019-09-23 08:01:33 +0300

    add a function that prevents wrong or no entry of data

--- js/script.js
+++ js/script.js
@@ -7,6 +7,19 @@ function getData(){
     date=parseInt(document.getElementById("date").value); 
 
 
+if(century==0){
+    alert("Please enter century");
+    return false;
+}else if (year<=0){
+    alert("Please enter year");
+    return false;
+}else if (month<=0 || month>12){
+    alert("Please enter month");
+    return false;
+}else if (date<=0 || date>31){
+    alert("Please enter date");
+    return false;
+}
 
 
 
