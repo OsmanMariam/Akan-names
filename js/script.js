e08f44b74cad122895530f21c497dd44b71b9179
OsmanMariam <sufimariam8@gmail.com>
2019-09-23 07:56:37 +0300

    set century,year,month and date variables

--- js/script.js
+++ js/script.js
@@ -1,36 +1,9 @@
 var century, year, month , date;
 
-function getData(){
-    century=parseInt(document.getElementById("century").value);
-    year=parseInt(document.getElementById("year").value);
-    month=parseInt(document.getElementById("month").value);
-    date=parseInt(document.getElementById("date").value); 
 
 
-if(century==0){
-    alert("Please enter century");
-    return false;
-}else if (year<=0){
-    alert("Please enter year");
-    return false;
-}else if (month<=0 || month>12){
-    alert("Please enter month");
-    return false;
-}else if (date<=0 || date>31){
-    alert("Please enter date");
-    return false;
-}
 
 
-function getDay(){
-    getData();
-    weekDay=((((century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + date) % 7);
-    console.log(dayOfWeek);
-    return(math.floor(dayOfWeek));
-    if (dayOfWeek<0) {
-        dayOfWeek= dayOfWeek * -1;
-    }else if (dayOfWeek>0){
-        return dayOfWeek;
-    }
-}
+
+
 
