bd0341e38692b635f64794dc063d8e62eac32a9c
OsmanMariam <sufimariam8@gmail.com>
2019-09-23 07:58:50 +0300

    create a function to convert input into intergers

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 var century, year, month , date;
 
-
+function getData(){
+    century=parseInt(document.getElementById("century").value);
+    year=parseInt(document.getElementById("year").value);
+    month=parseInt(document.getElementById("month").value);
+    date=parseInt(document.getElementById("date").value); 
 
 
 
